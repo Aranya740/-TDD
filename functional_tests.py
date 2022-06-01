31b14a0555f4c9ccb67d0b1dca315e95d9f8af06
Aranya740 <1172024073@qq.com>
2022-06-01 20:34:37 +0800

    first check

--- functional_tests.py
+++ functional_tests.py
@@ -28,18 +28,17 @@ class NewVisitorTest(unittest.TestCase):
             )
 
         inputbox.send_keys('Buy peacock feathers')
-
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1：Buy peacock feathers')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        self.check_for_row_in_list_table('1：Buy peacock feathers')
-        self.check_for_row_in_list_table('2：Use peacock feathers to make a fly')
+        #self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -47,11 +46,8 @@ class NewVisitorTest(unittest.TestCase):
            #  any(row.text == '1:Buy peacock feathers' for row in rows),
            # f"New to-do item did not appear in table. Contents were:\n{table.text}"
            # )
-        self.assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-            '2：Use peacock feathers to make a fly',
-            [row.text for row in rows]
-            )
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
         self.fail('Finish the test!')
        
 
