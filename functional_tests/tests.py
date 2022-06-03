59cc893f2bc5ae372a02cfec05cbcc3b6f053bde
Aranya740 <1172024073@qq.com>
2022-06-03 15:59:52 +0800

    chaptet 7 mid

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -21,13 +21,14 @@ class NewVisitorTest(LiveServerTestCase):
                 self.assertIn(row_text,[row.text for row in rows])
                 return
             except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time>MAX_WAIT:
+                if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
         
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
+        #time.sleep(15)
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do',header_text)
@@ -46,23 +47,20 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        #self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-           #  any(row.text == '1:Buy peacock feathers' for row in rows),
-           # f"New to-do item did not appear in table. Contents were:\n{table.text}"
-           # )
         self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
         self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
         self.fail('Finish the test!')
 
-    def test_can_start_a_list_for_one_user(self):
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+    #def test_can_start_a_list_for_one_user(self):
+        #self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        #self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    
+    
     def test_multiple_users_can_start_lists_at_different_urls(self):
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -75,7 +73,8 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
-
+        
+        self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertNotIn('make a fly',page_text)
@@ -92,7 +91,7 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
         self.assertNotIn('Buy milk',page_text)
-        
+     
        
 '''
 if __name__=='__main__':
